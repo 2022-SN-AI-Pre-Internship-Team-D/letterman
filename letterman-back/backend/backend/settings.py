86035de4e5517f8a3dad1a85f9675fc102933e37
Ellie <doris0707@naver.com>
2022-09-28 01:34:13 +0900

    goooood

--- letterman-back/backend/backend/settings.py
+++ letterman-back/backend/backend/settings.py
@@ -125,7 +125,7 @@ DATABASES = {
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://0.0.0.0:3000',
                          'http://127.0.0.1:8080', 'http://localhost:8080', 'http://0.0.0.0:8080',
                          'http://127.0.0.1:80', 'http://localhost:80', 'http://0.0.0.0:80',
-                         'http://localhost', 'http://0.0.0.0', 'http://127.0.0.1', 'http://127.0.0.1:5000', 'http://localhost:5000', 'http://0.0.0.0:5000',]
+                         'http://localhost', 'http://0.0.0.0', 'http://127.0.0.1',]
 CORS_ALLOW_CREDENTIALS = True
 
 # Password validation
