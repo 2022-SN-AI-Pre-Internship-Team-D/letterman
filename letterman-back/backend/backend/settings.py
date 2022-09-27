8a8a58093bba706313afbd1d29e1e0650e9161a5
Ellie <doris0707@naver.com>
2022-09-27 10:50:43 +0900

    nginx

--- letterman-back/backend/backend/settings.py
+++ letterman-back/backend/backend/settings.py
@@ -162,9 +162,14 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/mediafiles/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ["backend","localhost","frontend"]
--- letterman-back/backend/backend/urls.py
+++ letterman-back/backend/backend/urls.py
@@ -5,6 +5,9 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import AllowAny
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 schema_view_v1 = get_schema_view(
     openapi.Info(
         title="letterman APIs",
@@ -26,3 +29,6 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
