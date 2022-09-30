0eaa05dcf13fea8932dac7268a0c80d77fe46f83
jinhobyeong <jhb_0921@naver.com>
2022-10-01 02:27:23 +0900

    chore : link copy update 4

--- letterman-front/src/utils/useCopyClipBoard.tsx
+++ letterman-front/src/utils/useCopyClipBoard.tsx
@@ -6,43 +6,21 @@ function useCopyClipBoard(): [boolean, onCopyFn] {
   const [isCopy, setIsCopy] = useState<boolean>(false);
 
   const onCopy: onCopyFn = async (text: string) => {
-    if (navigator.clipboard !== undefined) {
-      navigator.clipboard.writeText(text).then(() => {
-        console.log('text가 복사됨');
-      });
-      return true;
-    }
-    // execCommand 사용
     const textArea = document.createElement('textarea');
     textArea.value = text;
     document.body.appendChild(textArea);
+    textArea.focus();
     textArea.select();
-    textArea.setSelectionRange(0, 99999);
     try {
       document.execCommand('copy');
-      setIsCopy(true);
+      document.body.removeChild(textArea);
+      return true;
     } catch (err) {
-      console.error('복사 실패', err);
-      setIsCopy(false);
+      console.error('Unable to copy to clipboard', err);
+      document.body.removeChild(textArea);
     }
-    textArea.setSelectionRange(0, 0);
-    document.body.removeChild(textArea);
-    console.log('text가 복사됨');
 
     return true;
-
-    // try {
-    //   await window.navigator.clipboard.writeText(text);
-    //   setIsCopy(true);
-    //   console.log('gooood');
-
-    //   return true;
-    // } catch (error) {
-    //   console.error(error);
-    //   setIsCopy(false);
-
-    //   return false;
-    // }
   };
 
   return [isCopy, onCopy];
