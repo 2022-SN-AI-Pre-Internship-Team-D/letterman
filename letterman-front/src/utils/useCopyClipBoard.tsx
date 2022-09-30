91f9467ebbfe49abf9aae71fc02c71f6d3052578
jinhobyeong <jhb_0921@naver.com>
2022-10-01 04:31:28 +0900

    chore : undo link copy

--- letterman-front/src/utils/useCopyClipBoard.tsx
+++ letterman-front/src/utils/useCopyClipBoard.tsx
@@ -6,21 +6,18 @@ function useCopyClipBoard(): [boolean, onCopyFn] {
   const [isCopy, setIsCopy] = useState<boolean>(false);
 
   const onCopy: onCopyFn = async (text: string) => {
-    const textArea = document.createElement('textarea');
-    textArea.value = text;
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
     try {
-      document.execCommand('copy');
-      document.body.removeChild(textArea);
-      return true;
-    } catch (err) {
-      console.error('Unable to copy to clipboard', err);
-      document.body.removeChild(textArea);
-    }
+      await window.navigator.clipboard.writeText(text);
+      setIsCopy(true);
+      console.log('gooood');
 
-    return true;
+      return true;
+    } catch (error) {
+      console.error(error);
+      setIsCopy(false);
+
+      return false;
+    }
   };
 
   return [isCopy, onCopy];
